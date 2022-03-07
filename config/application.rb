5a9d1a085acee113a13d1dd6c3af5f8583b30d09
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-03-07 16:19:48 -0700

    Content for c2s02 (2022-03-07)

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Sportsball
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.paths.add 'app/packages', glob: '*/{*,*/concerns}', eager_load: true
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,3 +37,20 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+# Adjust RSpec configuration for package folder structure
+RSpec.configure do |config|
+  config.define_derived_metadata(file_path: Regexp.new('/spec/packages/.*/controllers')) { |metadata| metadata[:type] = :controller }
+  config.define_derived_metadata(file_path: Regexp.new('/spec/packages/.*/models')) { |metadata| metadata[:type] = :model }
+  config.define_derived_metadata(file_path: Regexp.new('/spec/packages/.*/requests')) { |metadata| metadata[:type] = :request }
+  config.define_derived_metadata(file_path: Regexp.new('/spec/packages/.*/routing')) { |metadata| metadata[:type] = :routing }
+  config.define_derived_metadata(file_path: Regexp.new('/spec/packages/.*/system')) { |metadata| metadata[:type] = :system }
+  config.define_derived_metadata(file_path: Regexp.new('/spec/packages/.*/views')) { |metadata| metadata[:type] = :view }
+
+  config.before(:each, :type => lambda {|v| v == :view}) do
+    Dir.glob(Rails.root + ('app/packages/*/views')).each do |path|
+      view.lookup_context.view_paths.push path
+    end
+  end
+end
+
