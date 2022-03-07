b7e1c1c54ecf9670948b4bd63463261d14e71942
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-03-07 16:19:54 -0700

    Content for c2s05 (2022-03-07)

--- app/packages/rails_shims/controllers/application_controller.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationController < ActionController::Base
-  append_view_path(Dir.glob(Rails.root.join('app/packages/*/views')))
-end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -54,3 +54,20 @@ RSpec.configure do |config|
   end
 end
 
+
+# Adjust RSpec configuration for package folder structure
+RSpec.configure do |config|
+  config.define_derived_metadata(file_path: Regexp.new('/packages/.*/spec/controllers')) { |metadata| metadata[:type] = :controller }
+  config.define_derived_metadata(file_path: Regexp.new('/packages/.*/spec/models')) { |metadata| metadata[:type] = :model }
+  config.define_derived_metadata(file_path: Regexp.new('/packages/.*/spec/requests')) { |metadata| metadata[:type] = :request }
+  config.define_derived_metadata(file_path: Regexp.new('/packages/.*/spec/routing')) { |metadata| metadata[:type] = :routing }
+  config.define_derived_metadata(file_path: Regexp.new('/packages/.*/spec/system')) { |metadata| metadata[:type] = :system }
+  config.define_derived_metadata(file_path: Regexp.new('/packages/.*/spec/views')) { |metadata| metadata[:type] = :view }
+
+  config.before(:each, :type => lambda {|v| v == :view}) do
+    Dir.glob(Rails.root + ('packages/*/app/views')).each do |path|
+      view.lookup_context.view_paths.push path
+    end
+  end
+end
+
