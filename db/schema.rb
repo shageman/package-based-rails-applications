77a23c13e0a89d60aee3762608e8917aa5c1f55b
Stephan Hagemann <stephan.hagemann@gmail.com>
2022-03-11 06:11:23 -0700

    Content for c6s02 (2022-03-11)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_121418) do
+ActiveRecord::Schema.define(version: 2022_03_11_015107) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "date", precision: 6
